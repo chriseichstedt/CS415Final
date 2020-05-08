8bcf4866b442c55c58e460bbe5617e1262ede138
orion996 <42749590+orion996@users.noreply.github.com>
2020-05-08 11:17:16 -0700

    Update test.sh

--- azure/test.sh
+++ azure/test.sh
@@ -1,1 +1,5 @@
-echo "Hello World!" >> output.stdout
+ export PATH="../../upcxx2020/bin/:$PATH"
+ ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 3 >> res.stdout
+ ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 3 >> res.stdout
+ ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 3 >> res.stdout
+ ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 3 >> res.stdout
