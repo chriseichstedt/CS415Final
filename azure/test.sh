75cfbb3fef4bf2bd788e92050c9481a19cc32083
orion996 <42749590+orion996@users.noreply.github.com>
2020-05-08 11:35:59 -0700

    Update test.sh

--- azure/test.sh
+++ azure/test.sh
@@ -1,5 +1,5 @@
  export PATH="../../upcxx2020/bin/:$PATH"
- ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 3 >> res.stdout
- ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 3 >> res.stdout
- ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 3 >> res.stdout
- ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 3 >> res.stdout
+ UPCXX_SEGMENT_MB=1024 GASNET_MAX_SEGSIZE=1024M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 3 >> res.stdout
+ UPCXX_SEGMENT_MB=1024 GASNET_MAX_SEGSIZE=1024M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 3 >> res.stdout
+ UPCXX_SEGMENT_MB=1024 GASNET_MAX_SEGSIZE=1024M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 3 >> res.stdout
+ UPCXX_SEGMENT_MB=1024 GASNET_MAX_SEGSIZE=1024M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 3 >> res.stdout
