3dcb395ab1743c1e3b392567265d2859c3c88a90
chriseichstedt <44278316+chriseichstedt@users.noreply.github.com>
2020-05-09 10:40:23 -0700

    Update test.sh
    Fixed the stdout file for 100 matrix size

--- azure/test.sh
+++ azure/test.sh
@@ -11,7 +11,7 @@
  UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 50 >> res50.stdout
  UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 50 >> res50.stdout
  UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 50 >> res50.stdout
- UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 100 >> res50.stdout
- UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 100 >> res50.stdout
- UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 100 >> res50.stdout
- UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 100 >> res50.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 100 >> res100.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 100 >> res100.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 100 >> res100.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 100 >> res100.stdout
