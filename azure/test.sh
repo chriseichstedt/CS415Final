44dfa9ef09f8ff9d38249f4cc8b09775727faedb
chriseichstedt <44278316+chriseichstedt@users.noreply.github.com>
2020-05-09 13:17:59 -0700

    Update test.sh
    Added a make command for faster testing

--- azure/test.sh
+++ azure/test.sh
@@ -1,4 +1,5 @@
  export PATH="../../upcxx2020/bin/:$PATH"
+ make
  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 10 >> res10.stdout
  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 10 >> res10.stdout
  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 10 >> res10.stdout
