91743d3430d6317f133dfbbde2c3b2316c686db7
Ubuntu <eichstedtjordy@myvm.cirvp1z0vd1e1eqixypsupjoke.dx.internal.cloudapp.net>
2020-05-09 17:37:51 +0000

    05/09/20 results

--- azure/test.sh
+++ azure/test.sh
@@ -1,13 +1,13 @@
  export PATH="../../upcxx2020/bin/:$PATH"
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 10 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 10 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 10 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 10 >> res.stdout
- UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 100 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 100 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 100 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 100 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 1000 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 1000 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 1000 >> res.stdout
- #UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 1000 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 10 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 10 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 10 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 10 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 25 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 25 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 25 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 25 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 50 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 50 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 50 >> res.stdout
+ UPCXX_SEGMENT_MB=32000 GASNET_MAX_SEGSIZE=32000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 50 >> res.stdout
