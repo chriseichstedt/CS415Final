09fd6a746ce7356dc98f9bd61347c9b1e1da7f8d
Ubuntu <eichstedtjordy@myvm.cirvp1z0vd1e1eqixypsupjoke.dx.internal.cloudapp.net>
2020-05-10 15:52:42 +0000

    05/09/20 results #3

--- azure/test.sh
+++ azure/test.sh
@@ -1,8 +1,7 @@
  export PATH="../../upcxx2020/bin/:$PATH"
- make
  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 10 >> res10.stdout
  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 10 >> res10.stdout
- UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 10 >> res10.stdout
+ UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 4 ./upcxx_matmul 10 >> res10.stdout
  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 8 ./upcxx_matmul 10 >> res10.stdout
  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 1 ./upcxx_matmul 25 >> res25.stdout
  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 2 ./upcxx_matmul 25 >> res25.stdout
