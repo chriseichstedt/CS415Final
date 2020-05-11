713bea0d150527a644efe2eb8508a61bc1761ee9
chriseichstedt <44278316+chriseichstedt@users.noreply.github.com>
2020-05-11 10:21:10 -0700

    Update test.sh
    Removed unnecessary export lines

--- azure/test.sh
+++ azure/test.sh
@@ -1,9 +1,7 @@
- export PATH="../../upcxx2020/bin/:$PATH"
- export UPCXX_GASNET_CONDUIT=udp
- UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 1 -N 1 ./upcxx_matmul 10 >> res10.stdout
- UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 2 -N 2 ./upcxx_matmul 10 >> res10.stdout
- UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 4 -N 4 ./upcxx_matmul 10 >> res10.stdout
- UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 8 -N 8 ./upcxx_matmul 10 >> res10.stdout
+UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 1 -N 1 ./upcxx_matmul 10 >> res10.stdout
+UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 2 -N 2 ./upcxx_matmul 10 >> res10.stdout
+UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 4 -N 4 ./upcxx_matmul 10 >> res10.stdout
+UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 8 -N 8 ./upcxx_matmul 10 >> res10.stdout
 #  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 1 -N 1 ./upcxx_matmul 25 >> res25.stdout
 #  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 2 -N 2 ./upcxx_matmul 25 >> res25.stdout
 #  UPCXX_SEGMENT_MB=64000 GASNET_MAX_SEGSIZE=64000M ../../upcxx2020/bin/upcxx-run -n 4 -N 4 ./upcxx_matmul 25 >> res25.stdout
